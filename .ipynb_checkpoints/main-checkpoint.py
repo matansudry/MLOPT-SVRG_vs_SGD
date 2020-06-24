d6bef6ff707d55bcf218f01d49aa467aa36d99c7
matansudry <matansudry@gmail.com>
2020-06-24 14:33:08 +0300

    final upload

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,23 +1,20 @@
 import numpy as np
 from tqdm import tqdm
-from scipy.stats import ortho_group
 import math
 import random
 import matplotlib.pyplot as plt
 
 #parameters
-max_range = 100
+max_range = 75
 min_range = 50
-rows = 100
-columns = 100
-num_of_simulations = 1
-iterations = 1000
+rows = columns = 50
+num_of_simulations = 5
+iterations = 10000
 dim = min(rows, columns)
 b_noise_mean = 0
 b_noise_sd = 1
 x_noise_mean = 0
-x_noise_sd = 10
-data_repeat = 10
+x_noise_sd = 1
 
 #creating the matrix based on the parameters we get
 def creating_matrix(max_range, min_range, rows, columns, x_noise_mean, x_noise_sd, b_noise_mean, b_noise_sd):
@@ -66,15 +63,15 @@ def gradient(A, x, b):
 #ploting the results
 def plot(SGD, SGD5, SGD20, SGD40, SVRG, SVRG_pass,  iterations, sim):
     if SGD is not None:
-        plt.plot(range(1, iterations+1), SGD / iterations * sim, 'green')
+        plt.plot(range(1, iterations+1), SGD /  sim, 'green')
     if SGD5 is not None:
-        plt.plot(range(1, iterations+1), SGD5 / iterations * sim, 'red')
+        plt.plot(range(1, iterations+1), SGD5 /  sim, 'red')
     if SGD20 is not None:
-        plt.plot(range(1, iterations+1), SGD20 / iterations * sim, 'blue')
+        plt.plot(range(1, iterations+1), SGD20 /  sim, 'blue')
     if SGD40 is not None:
-        plt.plot(range(1, iterations+1), SGD40 / iterations * sim, 'grey')
+        plt.plot(range(1, iterations+1), SGD40 /  sim, 'grey')
     if SVRG is not None:
-        plt.plot(SVRG_pass, SVRG / iterations * sim, 'black')
+        plt.plot(SVRG_pass, SVRG /  sim, 'black')
 
     plt.legend(['SGD', 'SGD5', 'SGD20'
                 , 'SGD40', 'SVRG'], loc='best')
@@ -90,10 +87,9 @@ def plot(SGD, SGD5, SGD20, SGD40, SVRG, SVRG_pass,  iterations, sim):
 #batch Stochastic_gradient_descent
 def Stochastic_gradient_descent(iterations, rows, columns, matrix, matrix_with_noise, alpha, dim, beta, batch_size):
     scores_SGD = np.zeros(iterations)
-    x_sum = 0
     x = np.zeros(rows)
     scores_SGD[0] += loss(matrix, x, matrix_with_noise)
-    x_sum += x
+    x_sum = [x]
     for i in range(1, iterations):
         for j in range(int(rows/batch_size)):
             rand_indexes = np.random.randint(0, rows - 1, batch_size)
@@ -101,37 +97,43 @@ def Stochastic_gradient_descent(iterations, rows, columns, matrix, matrix_with_n
             b_tilda = matrix_with_noise[rand_indexes]
             step = 2/(alpha*(j+1))
             x = x - step * gradient(A_tilda,x,b_tilda)/batch_size
-        x_sum += x
-        x_avg = x_sum/i
+        x_sum.append(x)
+        x_avg = sum(x_sum)/len(x_sum)
         scores_SGD[i] += loss(matrix, x_avg, matrix_with_noise)
     return scores_SGD
 
 #SVRG
-def SVRG_function(iterations, rows, columns, matrix, matrix_with_noise, alpha, dim, data_repeat, beta, batch_size):
-    number_of_iterations = int((iterations / (data_repeat + 1)) + 1)
-    x_sum = 0
+def SVRG_function(iterations, rows, columns, matrix, matrix_with_noise, alpha, dim, beta):
+    data_repeat = int(200 * beta / alpha)
+    number_of_iterations = iterations - int(2 * data_repeat/rows)
     scores_SVRG = np.zeros(number_of_iterations)
     SVRG_pass = []
-    x = np.zeros(rows)
-    scores_SVRG[0] += loss(matrix, x, matrix_with_noise)
+    x_prev = np.zeros(rows)
+    x_i = [x_prev]
+    scores_SVRG[0] = loss(matrix, x_prev, matrix_with_noise)
     SVRG_pass.append(1)
-    x_sum += x
     for i in range(1,number_of_iterations):
-        x_prev = x
+        if (i > 1):
+            x_prev = sum(list_x)/len(list_x)
+        x_j = x_prev
+        step = 1 / (10*beta)
+        list_x = [x_j]
         gradient_score = gradient(matrix, x_prev, matrix_with_noise) / rows
-        for j in range(int(data_repeat * rows / batch_size)):
-            step = 1 / beta
+        for j in range(data_repeat):            
             #get random i's
-            rand_indexes = np.random.randint(0, rows - 1, batch_size)
+            rand_indexes = np.random.randint(0, rows - 1, 1)
             A_tilda = matrix[rand_indexes, :]
             b_tilda = matrix_with_noise[rand_indexes]
             # w(t)=w(t-1)-step(GRD(W(T-1)-GRD(W=y)+Mu)
-            x = x - step * (gradient(A_tilda, x, b_tilda) / batch_size - gradient(A_tilda, x_prev, b_tilda) / batch_size + gradient_score)
-        x_sum += x
-        SVRG_pass.append(i * data_repeat + i)
-        x_avg = x_sum / i
-        scores_SVRG[i] += loss(matrix, x_avg, matrix_with_noise)
-    return scores_SVRG, SVRG_pass
+            x_j = x_j - step * (gradient(A_tilda, x_j, b_tilda) - gradient(A_tilda, x_prev, b_tilda) + gradient_score)
+            list_x.append(x_j)
+        x_i.append(x_j)
+        SVRG_pass.append(i + int(2*data_repeat/rows))
+        x_avg = sum(x_i)/len(x_i)
+        scores_SVRG[i] = loss(matrix, x_avg, matrix_with_noise)
+        scores_SVRG_final = np.zeros(number_of_iterations)
+        scores_SVRG_final [:len(scores_SVRG)] += scores_SVRG
+    return scores_SVRG_final, SVRG_pass
 
 def main():
     for i in tqdm(range(num_of_simulations),total=num_of_simulations):
@@ -142,7 +144,7 @@ def main():
         SGD5 = Stochastic_gradient_descent(iterations, rows, columns, matrix, matrix_with_noise, alpha, dim, beta, batch_size=5)
         SGD20 = Stochastic_gradient_descent(iterations, rows, columns, matrix, matrix_with_noise, alpha, dim, beta, batch_size=20)
         SGD40 = Stochastic_gradient_descent(iterations, rows, columns, matrix, matrix_with_noise, alpha, dim, beta, batch_size=40)
-        SVRG, SVRG_pass = SVRG_function(iterations, rows, columns, matrix, matrix_with_noise, alpha, dim, data_repeat, beta, batch_size=5)
+        SVRG, SVRG_pass = SVRG_function(iterations, rows, columns, matrix, matrix_with_noise, alpha, dim, beta)
 
     plot(SGD, SGD5, SGD20, SGD40, SVRG, SVRG_pass,  iterations, num_of_simulations)
 
